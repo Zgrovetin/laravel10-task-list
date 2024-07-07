50885ba0d9e0fc596ef6034c0efcbda98a25cab7
Zgrovetin <534477@gmail.com>
2024-07-07 14:52:49 +0200

    04. Blade directives
    Passing an array to the main & the dynamic routes, giving the routes conventional names by pre/suffixes, if foreach forelse, generating URLs using the route function by passing the name and parameters.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,14 @@
-Hello, I'm a blade template!
-@isset($name)
-    <div>The name is: {{ $name }}</div>
-@endisset
+<h1>
+    The list of tasks
+</h1>
+<div>
+{{--    @if(count($tasks))--}}
+    @forelse($tasks as $task)
+        <div>
+            <a href="{{route('tasks.show', ['id' => $task->id])}}">{{$task->title}}</a>
+        </div>
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
+{{--    @endif--}}
+</div>

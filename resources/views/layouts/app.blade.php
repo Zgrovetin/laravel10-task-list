8b951fbcd87e7bd0b073578789c67dfbc3d7db3c
Zgrovetin <534477@gmail.com>
2024-07-08 08:59:14 +0200

    05. Layouts using template inheritance
    Extending sections (title & content) from one layout template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Laravel 10 task list app</title>
+    </head>
+
+    <body>
+        <h1>@yield('title')</h1>
+        <div>
+            @yield('content')
+        </div>
+    </body>
+
+</html>

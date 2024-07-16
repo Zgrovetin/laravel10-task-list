797b4d18d0763429945120fb77f22f726266c7f0
Zgrovetin <534477@gmail.com>
2024-07-16 09:34:49 +0200

    13. Sessions, errors and flash messages
    Sessions intro, validation error storage inside sessions, add and display a flash messages inside the session.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,11 +2,16 @@
 <html>
     <head>
         <title> Laravel 10 task list app</title>
+        @yield('styles')
     </head>
 
     <body>
         <h1>@yield('title')</h1>
         <div>
+            @if (session()->has('success'))
+                <div>{{ session('success') }}</div>
+            @endif
+
             @yield('content')
         </div>
     </body>

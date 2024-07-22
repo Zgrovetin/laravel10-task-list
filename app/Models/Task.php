69aa99d7142428735ea7ba8da59f5e93461e1996
Zgrovetin <534477@gmail.com>
2024-07-22 17:35:46 +0200

    20. Toggling task state
    completed - not completed

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,6 +10,10 @@ class Task extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'description', 'long_description'];
-//    protected $guarded = ['secret'];
 
+    public function toggleComplete()
+    {
+        $this->completed = !$this->completed;
+        $this->save();
+    }
 }
